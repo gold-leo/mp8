241314bb4fae645e6b3615652b010dedec8d3d9b
gold-leo <goldmanleo@grinnell.edu>
2024-11-13 21:58:23 -0600

    Implemented MergeSorter

--- src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
@@ -1,6 +1,9 @@
 package edu.grinnell.csc207.sorting;
 
 import java.util.Comparator;
+import  java.lang.reflect.Array;
+
+import edu.grinnell.csc207.util.ArrayUtils;
 
 /**
  * Something that sorts using merge sort.
@@ -55,6 +58,47 @@ public class MergeSorter<T> implements Sorter<T> {
    */
   @Override
   public void sort(T[] values) {
-    // STUB
+    sortHelper(values, 0, values.length - 1);
   } // sort(T[])
+
+  /**
+   * 
+   * @param v
+   * @param start
+   * @param end
+   */
+  private void sortHelper(T[] v, int start, int end) {
+    int len = end - start;
+    if (len == 0) {
+      return;
+    } else if (len == 1) {
+      if (order.compare(v[start], v[end]) > 0) {
+        ArrayUtils.swap(v, start, end);
+      }
+    } else {
+      int halfLen = (len / 2) + start;
+      sortHelper(v, start, halfLen);
+      sortHelper(v, halfLen + 1, end);
+
+      T[] arr = (T[]) Array.newInstance(v[0].getClass(), len + 1);
+      System.err.println("merge sort begin. size: " + arr.length);
+      System.err.println("start: " + start + " halfLen: " + halfLen + " end: " + end);
+      for (int i = 0, x = start, y = halfLen + 1; i < arr.length; i++) {
+        System.err.println("index: " + i);
+        System.err.println("x: " + x + " y: " + y);
+        if (x > halfLen) {
+          arr[i] = v[y++];
+        } else if (y == arr.length + start) {
+          arr[i] = v[x++];
+        } else if (order.compare(v[x], v[y]) < 0) {
+          arr[i] = v[x++];
+        } else {
+          arr[i] = v[y++];
+        }
+      }
+      for (int i = 0; i < arr.length; i++) {
+        v[i + start] = arr[i];
+      }
+    } // if/if/else
+  }
 } // class MergeSorter
