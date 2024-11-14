9b4150f1aecdfb2745f100283af624b48263e087
gold-leo <goldmanleo@grinnell.edu>
2024-11-13 22:24:47 -0600

    Proper commenting for MergeSorter

--- src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
@@ -62,43 +62,58 @@ public class MergeSorter<T> implements Sorter<T> {
   } // sort(T[])
 
   /**
+   * Recursive function for sort(). Sorts array v within the bounds given.
    * 
    * @param v
+   *    The array of values.
    * @param start
+   *    The starting bound (inclusive).
    * @param end
+   *    The ending bound (inclusive).
    */
   private void sortHelper(T[] v, int start, int end) {
+    // Find the length - 1 of the bounds given.
     int len = end - start;
+
+    // Depending on the size, sort with a particular method.
+    // If len = 0 (size = 1), do nothing (already sorted).
+    // If len = 2 (size = 2), sort by comparing and flipping.
+    // Otherwise, split the area between the bounds and sort them, then merge.
     if (len == 0) {
       return;
     } else if (len == 1) {
       if (order.compare(v[start], v[end]) > 0) {
         ArrayUtils.swap(v, start, end);
-      }
+      } // if
     } else {
+      // halfLen is the separating space between the two new areas.
       int halfLen = (len / 2) + start;
       sortHelper(v, start, halfLen);
       sortHelper(v, halfLen + 1, end);
-
-      T[] arr = (T[]) Array.newInstance(v[0].getClass(), len + 1);
-      System.err.println("merge sort begin. size: " + arr.length);
-      System.err.println("start: " + start + " halfLen: " + halfLen + " end: " + end);
-      for (int i = 0, x = start, y = halfLen + 1; i < arr.length; i++) {
-        System.err.println("index: " + i);
-        System.err.println("x: " + x + " y: " + y);
-        if (x > halfLen) {
-          arr[i] = v[y++];
-        } else if (y == arr.length + start) {
-          arr[i] = v[x++];
-        } else if (order.compare(v[x], v[y]) < 0) {
-          arr[i] = v[x++];
-        } else {
-          arr[i] = v[y++];
-        }
-      }
-      for (int i = 0; i < arr.length; i++) {
-        v[i + start] = arr[i];
-      }
+      merge(v, start, end, halfLen);
     } // if/if/else
-  }
+  } // sortHelper(T[], int, int)
+
+  private void merge(T[] v, int start, int end, int halfLen) {
+    // Create a new empty array with the size of the whole given area.
+    T[] arr = (T[]) Array.newInstance(v[0].getClass(), end - start + 1);
+
+    // Merge the two areas based off of the sorted rules.
+    for (int i = 0, x = start, y = halfLen + 1; i < arr.length; i++) {
+      if (x > halfLen) {
+        arr[i] = v[y++];
+      } else if (y == arr.length + start) {
+        arr[i] = v[x++];
+      } else if (order.compare(v[x], v[y]) < 0) {
+        arr[i] = v[x++];
+      } else {
+        arr[i] = v[y++];
+      } // if/if/if/else
+    } // for
+
+    // Now replace the given area in the original array with the new array's order.
+    for (int i = 0; i < arr.length; i++) {
+      v[i + start] = arr[i];
+    } // for
+  } // merge(T[], int, int, int)
 } // class MergeSorter
