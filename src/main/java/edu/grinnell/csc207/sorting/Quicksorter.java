63d9b0f34d62794bdea1dc37098d01b6251fe185
gold-leo <goldmanleo@grinnell.edu>
2024-11-14 22:16:53 -0600

    Implemented Quicksort

--- src/main/java/edu/grinnell/csc207/sorting/Quicksorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/Quicksorter.java
@@ -1,6 +1,9 @@
 package edu.grinnell.csc207.sorting;
 
 import java.util.Comparator;
+import java.util.Random;
+
+import edu.grinnell.csc207.util.ArrayUtils;
 
 /**
  * Something that sorts using Quicksort.
@@ -55,6 +58,42 @@ public class Quicksorter<T> implements Sorter<T> {
    */
   @Override
   public void sort(T[] values) {
-    // STUB
+    sortHelper(values, 0, values.length - 1);;
   } // sort(T[])
+
+  private void sortHelper(T[] v, int lb, int ub) {
+    if (ub - lb > 0) {
+
+      // Find a pivot. I chose the center.
+      int pivot = lb + (ub - lb) / 2;
+      ArrayUtils.swap(v, pivot, lb);
+
+      // Initialize the sorting indexes.
+      int sm = lb + 1;
+      int lg = ub;
+
+      // Check and sort each number.
+      for (; sm < lg;) {
+        if (order.compare(v[sm], v[lb]) < 1) {
+          sm++;
+        } else {
+          ArrayUtils.swap(v, sm, lg);
+          lg--;
+        }
+      }
+
+      // Final check
+      if (order.compare(v[sm], v[lb]) < 1) {
+        ArrayUtils.swap(v, lb, sm);
+        sm--;
+      } else {
+        sm--;
+        ArrayUtils.swap(v, lb, sm);
+        sm--;
+      }
+
+      sortHelper(v, lb, sm);
+      sortHelper(v, lg, ub);
+    }
+  }
 } // class Quicksorter
